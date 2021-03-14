cc58809aec854ca8bcfa726ea63890d92646837e
William Almeida <77554433+william-almeida@users.noreply.github.com>
2021-03-14 17:21:48 -0300

    update

--- challenges/003/script.js
+++ challenges/003/script.js
@@ -1,21 +1,37 @@
-const result = document.querySelector('section.result')
+const section = document.querySelector('section')
 const img = document.querySelector('img#currentHourImage')
-// background 
-const bgContainer = document.querySelector('div.container')
-const bgScetion = result.style.backgroungColor
+const result = document.querySelector('div#result')
+
+const bgColor = document.body
 
 const currentDate = new Date()
 const currentHour = currentDate.getHours()
 
-
-
-if (currentHour >= 6 && currentHour < 12) {
-    // morgning - manhã
+function settings(){
+  if (currentHour >= 6 && currentHour < 12) {
+    // set css
+    bgColor.style.background = "#bdbdbf"
+    section.style.background = "#ffffff"
+    // load img
     img.src= './images/morning.jpg'
-    bgContainer.style.background = "#f2f3f5"
-} else if (currentHour >= 12 && currentHour < 18) {
-    // afternoon
+    // html
+    result.innerHTML = `<p> agora são ${currentHour} horas</p>`
+
+  } else if (currentHour >= 12 && currentHour < 18) {
+    // set css
+    bgColor.style.background = "#f8b350"
+    section.style.background = "#ffffff"
+    // load img
     img.src= './images/afternoon.jpg'
-} else if (currentHour >= 18 && currentHour < 0) {
-    img.src= './images/night.jpg'
+    // html
+    result.innerHTML = `<p> agora são ${currentHour} horas</p>`
+  } else if (currentHour >= 18 && currentHour < 0) {
+    // set css
+    bgColor.style.background = "#f8b350"
+    section.style.background = "#ffffff"
+    // load img
+    img.src= './images/afternoon.jpg'
+    // html
+  }
 }
+
