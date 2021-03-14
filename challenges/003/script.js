5d9d22b46ccb86275a9312f3b259b9246c7728ce
William Almeida <77554433+william-almeida@users.noreply.github.com>
2021-03-14 17:49:54 -0300

    finish

--- challenges/003/script.js
+++ challenges/003/script.js
@@ -2,15 +2,15 @@ const section = document.querySelector('section')
 const img = document.querySelector('img#currentHourImage')
 const result = document.querySelector('div#result')
 
-const bgColor = document.body
+const bodyColor = document.body
 
 const currentDate = new Date()
-const currentHour = 8 //currentDate.getHours()
+const currentHour = 15 //currentDate.getHours()
 
 function settings(){
   if (currentHour >= 6 && currentHour < 12) {
     // set css
-    bgColor.style.background = "#bdbdbf"
+    bodyColor.style.background = "#bdbdbf"
     section.style.background = "#ffffff"
     // load img
     img.src= './images/morning.jpg'
@@ -19,19 +19,22 @@ function settings(){
 
   } else if (currentHour >= 12 && currentHour < 18) {
     // set css
-    bgColor.style.background = "#f8b350"
+    bodyColor.style.background = "#f8b350"
     section.style.background = "#ffffff"
     // load img
     img.src= './images/afternoon.jpg'
     // html
     result.innerHTML = `<p> agora são ${currentHour} horas</p>`
-  } else if (currentHour >= 18 && currentHour < 0) {
+  } else if (currentHour >= 18 && currentHour < 24) {
     // set css
-    bgColor.style.background = "#f8b350"
-    section.style.background = "#ffffff"
+    bodyColor.style.background = "#121214"
+    bodyColor.style.color = "#ffffff"
+    section.style.background = "#212025"
+    section.style.boxShadow = 'none'
     // load img
     img.src= './images/night.jpg'
     // html
+    result.innerHTML = `<p> agora são ${currentHour} horas</p>`
   }
 }
 
