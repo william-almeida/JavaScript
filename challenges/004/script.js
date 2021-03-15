59362883ce560a21181018a7e9ce0b23dd089d5e
William Almeida <77554433+william-almeida@users.noreply.github.com>
2021-03-15 08:00:25 -0300

    base

--- challenges/004/script.js
+++ challenges/004/script.js
@@ -1,12 +1,31 @@
 function loadPhoto() {
-  let birthYear = document.getElementById('birth-year').value
-  birthYear = Number(birthYear)
+  // Birth Year 
+  let birthYear = document.querySelector('#birth-year').value
+  birthYear = parseInt(birthYear)
   const dateToDay = new Date()
   const currentYear = dateToDay.getFullYear()
   const yearOld = currentYear - birthYear
-  console.log(yearOld)
-  // const birthYear = document.querySelector('#birth-year')
-  let res = document.body
-   
-  res.innerText = `${yearOld}`
+  
+  // Get image 
+  const image = document.querySelector("#profile")
+  let imageSource = "female-kid.jpg"
+
+  // Get gender
+  let gender = document.getElementsByName('gender')
+  if (gender[0].checked) {
+    if (yearOld <= 13 ) {
+      imageSource = "male-kid.jpg"
+      
+      console.log('okok')
+    }  
+  } else {
+    if (yearOld <= 13 ) {
+      imageSource = "female-kid.jpg"
+      
+      console.log('koko')
+    }  
+  }
+
+  image.src = `./images/${imageSource}`
+  
 }
