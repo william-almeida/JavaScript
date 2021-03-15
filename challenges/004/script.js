f02c85807222cef18a9aa8e67329c61b9fa9a9bb
William Almeida <77554433+william-almeida@users.noreply.github.com>
2021-03-15 18:27:34 -0300

    checking years old

--- challenges/004/script.js
+++ challenges/004/script.js
@@ -12,18 +12,55 @@ function loadPhoto() {
 
   // Get gender
   let gender = document.getElementsByName('gender')
-  if (gender[0].checked) {
-    if (yearOld <= 13 ) {
-      imageSource = "male-kid.jpg"
-      
-      console.log('okok')
-    }  
+
+  // certificando se o ano de nascimento é maior
+  if (birthYear > currentYear) {
+
+    window.alert("Por favor, digite um ano válido!!!")
+
   } else {
-    if (yearOld <= 13 ) {
-      imageSource = "female-kid.jpg"
-      
-      console.log('koko')
-    }  
+    // MALE
+    if (gender[0].checked) {
+      if (yearOld <= 13 ) {
+        imageSource = "male-kid.jpg"
+        // criança
+  
+      }  else if (yearOld <= 18) {
+        imageSource = "male-teen.jpg"
+        // adolescente
+  
+      } else if (yearOld <= 55) {
+        imageSource = "male-adult.jpg"
+        // adulto
+  
+      } else if (yearOld <= 120) {
+        imageSource = "male-old.jpg"
+        // idoso
+  
+      }
+
+    // FEMALE
+    } else {
+      if (yearOld <= 13 ) {
+        imageSource = "female-kid.jpg"
+        // criança
+  
+      }  else if (yearOld <= 18) {
+        imageSource = "female-teen.jpg"
+        // adolescente
+  
+      } else if (yearOld <= 55) {
+        imageSource = "female-adult.jpg"
+        // adulta
+  
+      } else if (yearOld <= 120) {
+        imageSource = "female-old.jpg"
+        // idosa
+        
+      } else {
+        
+      }
+    }
   }
 
   image.src = `./images/${imageSource}`
